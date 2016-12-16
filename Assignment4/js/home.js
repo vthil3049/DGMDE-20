5953c7fd42761ce81a10f55dba64056ca994bedb
Vani Thilagar <vthilagar@g.harvard.edu>
2016-12-16 12:46:17 -0500

    Completed jQuery mobile part of assignment

--- Assignment4/js/home.js
+++ Assignment4/js/home.js
@@ -4,25 +4,32 @@ $(document).ready(function(){
 	var image_list = [
 		{
 			id:0,
+			src:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Hasthamudra19.JPG/256px-Hasthamudra19.JPG",
+			alt:"Blossoming Flower",
+			desc:"Blossoming Flower"
+		},
+		
+		{
+			id:1,
 			src:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Hasthamudra11.JPG/256px-Hasthamudra11.JPG",
 			alt:"Hasthamudra11",
 			desc:"Pretty bird with crest"
 		},
 		{
-			id:1,
+			id:2,
 			src:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Hasthamudra12.JPG/256px-Hasthamudra12.JPG",
 			alt:"Hasthamudra12",
 			desc:"Cobra with its hood up"
 		},
 		{
-			id:2,
+			id:3,
 			src:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Hasthamudra2.JPG/256px-Hasthamudra2.JPG",
 			alt:"Hasthamudra2",
 			desc:"Warrior holding a spear"
 
 		},
 		{
-			id:3,
+			id:4,
 			src:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Hasthamudra9.JPG/256px-Hasthamudra9.JPG",
 			alt:"Hasthamudra9",
 			desc:"Pair of scissors"
@@ -31,24 +38,68 @@ $(document).ready(function(){
 	];
 
 	var currIndex = 0;
-
-	$('#hands').click(function(event) {
-		$(this).removeAttr('src');
-		$(this).removeAttr('alt');
-		$(this).removeAttr('title');
-		currIndex++;
-		if (currIndex > 3)
+	$("#hands").on("swiperight", function (e) {
+		//alert("you swiped right!");
+		removeAttributes(this);
+		if (currIndex == 0)
 		{
-			currIndex=0;
+			currIndex = 4;
+		}
+		else {
+			currIndex--;
 		}
 		var imageInfo = image_list[currIndex];
 		console.log(imageInfo);
-		$(this).attr('src', imageInfo.src);
-		$(this).attr('alt', imageInfo.alt);
-		$(this).attr('title', imageInfo.desc);
+		addAttributes(this, imageInfo);
 		$('#img_caption').text(imageInfo.desc);
 	});
 
+	$("#hands").on("swipeleft", function (e) {
+		//alert("you swiped left!");
+		removeAttributes(this);
+		if (currIndex == 4)
+		{
+			currIndex = 0;
+		}
+		else {
+			currIndex++;
+		}
+		var imageInfo = image_list[currIndex];
+		console.log(imageInfo);
+		addAttributes(this, imageInfo);
+		$('#img_caption').text(imageInfo.desc);
+	});
+
+	// $('#hands').click(function(event) {
+	// 	$(this).removeAttr('src');
+	// 	$(this).removeAttr('alt');
+	// 	$(this).removeAttr('title');
+	// 	currIndex++;
+	// 	if (currIndex > 3)
+	// 	{
+	// 		currIndex=0;
+	// 	}
+	// 	var imageInfo = image_list[currIndex];
+	// 	console.log(imageInfo);
+	// 	$(this).attr('src', imageInfo.src+"?"+ Math.random());
+	// 	$(this).attr('alt', imageInfo.alt);
+	// 	$(this).attr('title', imageInfo.desc);
+	// 	$('#img_caption').text(imageInfo.desc);
+	// });
+
+	function removeAttributes(element)
+	{
+		$(element).removeAttr('src');
+		$(element).removeAttr('alt');
+		$(element).removeAttr('title');
+	}
+
+	function addAttributes(element, imageInfo)
+	{
+		$(element).attr('src', imageInfo.src);
+		$(element).attr('alt', imageInfo.alt);
+		$(element).attr('title', imageInfo.desc);
+	}
 
 	//Form related functions and data
 	var classical = [
